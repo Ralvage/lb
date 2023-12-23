5a74711ea652c1903d8fc2b419484c78ad5e30f5
Ralvage <88508079+Ralvage@users.noreply.github.com>
2023-12-23 11:36:29 +0600

    Add files via upload

---
+++ SSH/main.py
@@ -0,0 +1,24 @@
+import time
+from pywinauto.application import Application
+import keyboard
+
+app = Application().start("C:\Games\putty.exe -ssh ralvage@tty.sdf.org")
+
+time.sleep(5)
+keyboard.write('YqgRUqQBsvdnMg')
+
+time.sleep(5)
+keyboard.send('enter')
+time.sleep(1)
+keyboard.send('enter')
+time.sleep(1)
+keyboard.send('enter')
+time.sleep(1)
+keyboard.send('enter')
+time.sleep(1)
+keyboard.send('enter')
+time.sleep(1)
+keyboard.send('enter')
+time.sleep(1)
+keyboard.send('enter')
+time.sleep(10)
